dc7a619b5e621b7ba45a68756884fa54b6948a99
Junjie Wu <clatisus@gmail.com>
2024-08-12 17:40:11 +0800

    add motion plugin flit.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -24,6 +24,14 @@ return {
   },
 
   {
+    "ggandor/flit.nvim",
+    lazy = false,
+    opts = {
+      use_no_labels = true,
+    },
+  },
+
+  {
     "kylechui/nvim-surround",
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
