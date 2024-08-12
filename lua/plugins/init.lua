bcd7e518925f318b2b6aa7d9ef6769081543a591
Junjie Wu <clatisus@gmail.com>
2024-08-12 16:31:27 +0800

    disable hardtime

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -85,6 +85,7 @@ return {
   {
     "m4xshen/hardtime.nvim",
     event = "VeryLazy",
+    enabled = false,
     dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
     opts = {},
   },
